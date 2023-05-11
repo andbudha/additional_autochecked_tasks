a565b4f9a8979693540da55e029ebb8d049adcb9
andbudha <andreibartov@gmail.com>
2023-05-11 21:59:12 +0200

    homework 5 updated/Routes added

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -48,7 +48,7 @@ export const friendMessage0: MessageType = {
         name: 'Dorian', // можно менять
     },
     message: {
-        text: 'Hey, jep...Why???', // можно менять
+        text: 'Hey, yep...Why???', // можно менять
         time: '22:00', // можно менять
     },
 }
