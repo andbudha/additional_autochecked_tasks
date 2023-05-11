82be956a6cc29dad2c99a28bd93f70d8f8ac9722
andbudha <andreibartov@gmail.com>
2023-05-11 19:05:07 +0200

    homework 4 updated/SuperInputComponent Updated

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import s from './App.module.css'
 import HW4 from "../s2-homeworks/hw04/HW4";
 
+
 function App() {
     return (
         <div className={s.App}>
             {/*<HW1/>*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
-
+            {/*<HW4/>*/}
             <HW4/>
+
 
             {/*раскомментировать по ходу выполнения*/}
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -43,7 +43,10 @@ const Stand = () => {
             <div className={s.buttons}>
                 {/*обычная кнопка:*/}
                 <div>
-                    <SuperButton id={'hw4-super-button-default'}>
+                    <SuperButton
+                        id={'hw4-super-button-default'}
+                        xType={'default'}
+                    >
                         default
                     </SuperButton>
                 </div>
@@ -57,7 +60,7 @@ const Stand = () => {
                 <div>
                     <SuperButton
                         id={'hw4-super-button-disabled'}
-                        xType={'red'}
+                        //xType={'disabled'}
                         disabled
                     >
                         disabled
@@ -90,7 +93,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                        onChangeChecked={setChecked}
                     />
                 </div>
             </div>
