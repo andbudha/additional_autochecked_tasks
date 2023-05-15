1d190ee59d1a25aca4916702cc43885fdb3cb712
andbudha <andreibartov@gmail.com>
2023-05-15 15:28:12 +0200

    homework 8 updated/ functionality added

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -6,11 +6,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: initialState, action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: initialState, action: ActionType):UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const newState = [...state].sort((a,b)=>{
+                if(a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0;
+            })
 
-            return state // need to fix
+            return action.payload === 'up' ? newState : newState.reverse();
         }
         case 'check': {
 
@@ -20,3 +25,6 @@ export const homeWorkReducer = (state: initialState, action: ActionType): UserTy
             return state
     }
 }
+
+
+
