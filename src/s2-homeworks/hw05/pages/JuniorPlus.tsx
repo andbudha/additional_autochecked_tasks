de93586628171609d1f93e573fa44e31208c083e
andbudha <andreibartov@gmail.com>
2023-05-23 22:52:36 +0200

    app updated/styles added

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HW10 from "../../hw10/HW10";
+import HW11 from '../../hw11/HW11';
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
@@ -17,6 +18,7 @@ function JuniorPlus() {
             {/*<HW14 />*/}
             {/*<HW15 />*/}
             <HW10 />
+            <HW11 />
         </div>
     )
 }
