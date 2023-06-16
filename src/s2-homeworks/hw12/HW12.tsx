e009735dd313fcbf614c6104b9ff8d5c9788c9c0
andbudha <andreibartov@gmail.com>
2023-06-16 14:45:43 +0200

    HW12 themeReducer updated

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,9 +1,11 @@
+
+
 import React, { useEffect } from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import { useDispatch, useSelector } from 'react-redux'
-import { changeThemeId } from './bll/themeReducer'
+import { ThemeStateType, changeThemeId } from './bll/themeReducer'
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
 const initState = {
     themeId: 1,
 }
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type ThemeStateType = typeof initState;
+type ActionType = changeThemeIdType;
+export const themeReducer = (state: ThemeStateType = initState, action: ActionType): ThemeStateType => { // fix any
     switch (action.type) {
-        // дописать
-
-        default:
+        case "SET_THEME_ID": {
+            return { ...state, themeId: action.id };
+        }
+        default: {
             return state
+        }
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export type changeThemeIdType = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) // fix any
